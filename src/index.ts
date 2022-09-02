02eceaa196c0446a9de47b0fb585c3e39f2e7f35
LeonMesquita <leonmesquita16@gmail.com>
2022-09-02 11:57:53 -0300

    project creation

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+app.use(cors());
+
+const PORT: number = Number(process.env.PORT) ||  5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
